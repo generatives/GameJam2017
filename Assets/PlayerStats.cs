98c9f72b59a3d4efe6e08acbf12d72822d5e16e2
Kevin Morrison <kwmorr@hotmail.com>
2017-11-18 16:28:19 -0800

    What do we do

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class PlayerStats {
+
+
+	public float firingRate;
+	public float projectileSpeed;
+	public float damage;
+	public float speed = 10.0f;
+	public float gravity = 10.0f;
+	public float jumpHeight = 2.0f;
+	public float healingRate;
+
+//	//public PlayerStats (){
+//	
+//	firingRate = 0;
+//	projectileSpeed = 0;
+//	damage = 0;
+//	speed = 10.0f;
+//	gravity = 10.0f;
+//	jumpHeight = 2.0f;
+//	healingRate = 0;
+//	
+//	
+//	}
+
+}
